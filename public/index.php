01002ab9d9dad352a8c67609c582e781028da193
Vikram Singh <vsingh7@gmail.com>
2020-11-21 03:37:56 +0000

    Updated index.php

--- public/index.php
+++ public/index.php
@@ -2,6 +2,38 @@
 
 require_once('../vendor/autoload.php');
 
-$app = new AssignmentFive\App();
+use AssignmentFive\Router\{
+	HtmlResponse,
+	JsonResponse,
+	Request,
+	Router
+};
 
-$app->dispatch();
+$requestMethod = $_POST['method'] ?? $_SERVER['REQUEST_METHOD'];
+$queryString = $_SERVER['QUERY_STRING'];
+$responseType = apache_request_headers()['Accept'];
+$parameters = [];
+
+switch ($requestMethod) {
+	case 'POST':
+		$parameters = $_POST;
+		break;
+	case 'PUT':
+		parse_str(file_get_contents("php://input"), $parameters);
+}
+
+$request = new Request($requestMethod, $queryString, $parameters);
+
+switch ($responseType) {
+	case 'application/json':
+		$response = new JsonResponse();
+		break;
+	default:
+		$response = new HtmlResponse();
+		break;
+}
+
+$router = new Router($request, $response);
+$response = $router->dispatch();
+
+print $response;
